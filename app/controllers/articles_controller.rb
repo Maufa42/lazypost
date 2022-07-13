a608328d993e9859f3315855bf439d7ac81892f8
Aryan Pal <aryanpalongraph@gmail.com>
2022-07-13 19:00:06 +0530

    📝 Bugged Staged

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,24 @@
 class ArticlesController < ApplicationController
   def index
+    @article_index = Article.all
   end
 
   def show
+    @article_show = Article.find(params[:id])
   end
 
   def new
+    @article_new = Article.new
   end
 
   def create
+    @article_new = Article.new(article_params)
+    debugger
+    if @article_new.save
+      redirect_to @article  
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -18,5 +28,10 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private 
+  def article_params
+    params.require(:article).permit(:title,:body)
   end
 end
